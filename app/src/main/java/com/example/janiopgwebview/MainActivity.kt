85800e61d7ada9db31b0f1dbddcaada8988d67d2
JanioPG <janiograce@gmail.com>
2022-10-18 22:41:10 -0300

    add Firebase SDK

--- app/src/main/java/com/example/janiopgwebview/MainActivity.kt
+++ app/src/main/java/com/example/janiopgwebview/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.janiopgwebview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.loadUrl("https://janiopg.github.io/The-Prancing-Pony/")
     }
 }
