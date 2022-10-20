aac303d22864da1c2e513ca7387e6dfc1a3284d7
JanioPG <janiograce@gmail.com>
2022-10-19 21:49:03 -0300

    add GTM

--- app/src/main/java/com/example/janiopgwebview/MainActivity.kt
+++ app/src/main/java/com/example/janiopgwebview/MainActivity.kt
@@ -3,11 +3,24 @@ package com.example.janiopgwebview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    // [START declare_analytics]
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+    // [END declare_analytics]
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // [START shared_app_measurement]
+        // Obtain the FirebaseAnalytics instance.
+        firebaseAnalytics = Firebase.analytics
+        // [END shared_app_measurement]
 
         val myWebView: WebView = findViewById(R.id.webview)
         myWebView.settings.javaScriptEnabled = true
